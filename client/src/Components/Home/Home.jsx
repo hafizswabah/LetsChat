8aa3b194c5e76bf27c7756be505d58a803ad45ff
hafizswabah <swabah.a.avd121@gmail.com>
2024-04-09 14:01:46 +0530

    make chats live as per the user input

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./Home.css"
 function Home() {
-    let chat = true;
-    let chatClassName = chat ? "chats" : "help-message"
+    let [chat, setChat] = useState("")
+    let [chatHistory, setChatHistory] = useState([])
+    const chatContainerRef = useRef()
+
+    function handleChat() {
+        if (!chat.trim() == "") {
+            setChatHistory(prevchats => [...prevchats, chat.trim()])
+            setChat = ""
+        }
+    }
+
+    let chatClassName = chatHistory.length === 0 ? "help-message" : "chats"
+    useEffect(() => {
+        chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight
+    }, [chatHistory])
+
     return (
         <>
             <div className="home-container">
@@ -12,56 +26,45 @@ function Home() {
                     </h2>
                 </div>
                 <div className="chat-section">
-                    <div className={chatClassName}>
-                        {chat ?
-                        <>
-                             <div className='chat-messages-container'>
-                                <div className="user-details">
-                                    <div className="userProfile">
-                                        <img src="/profileImage.avif" alt="" />
-                                    </div>
-                                    <div className="userName">
-                                        <h3>User name</h3>
-                                    </div>
-                                </div>
-                                <div className="userMessage">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id neque dui. Suspendisse potenti.
-                                        tellus mollis, eu tristique ipsum pellentesque.
+                    <div className={chatClassName} ref={chatContainerRef}>
+                        {chatHistory.length > 0 ?
+                            <>
+                                {chatHistory.map((item) => {
+                                    return (
+                                        <div className='chat-messages-container' >
+                                            <div className="user-details">
+                                                <div className="userProfile">
+                                                    <img src="/profileImage.avif" alt="" />
+                                                </div>
+                                                <div className="userName">
+                                                    <h5>User name</h5>
+                                                </div>
+                                            </div>
+                                            <div className="userMessage">
+                                                <p>
+                                                    {item}
+                                                </p>
+                                            </div>
+                                        </div>)
+                                })}
 
-                                    </p>
-                                </div>
-                            </div>
-                             <div className='chat-messages-container'>
-                                <div className="user-details">
-                                    <div className="userProfile">
-                                        <img src="/profileImage.avif" alt="" />
-                                    </div>
-                                    <div className="userName">
-                                        <h3>User name</h3>
-                                    </div>
-                                </div>
-                                <div className="userMessage">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id neque dui. Suspendisse potenti.
-                                        tellus mollis, eu tristique ipsum pellentesque.
 
-                                    </p>
-                                </div>
-                            </div>
-                         
-                     
-                        </>
-                       
+
+
+                            </>
+
                             : <h2>How can i help you today ?</h2>
                         }
                     </div>
                 </div>
                 <div className="chat-box-section">
                     <div className="chat-box">
-                        <input type="text" placeholder="message to Let's chat bot" />
+                        <input type="text" placeholder="message to Let's chat bot" onChange={(e) => setChat(e.target.value)} />
+                        <button className='chatSendBtn' onClick={handleChat}>send</button>
                     </div>
                 </div>
 
-            </div>
+            </div >
         </>
     )
 }
